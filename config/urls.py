e8468a390b196c6ae324c99b0eb456a1df1188a1
cristhoffer <cristhofferperezgaray@gmail.com>
2022-12-30 01:32:54 -0600

    Inicio de ejecución

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('apps.core.urls', namespace='core')),
+    path('', include('apps.usuarios.urls', namespace='usuarios')),
     path('admin/', admin.site.urls),
 ]
 
