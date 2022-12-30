d3f68067ef965f8aae022154e3cd8052b81bfba8
cristhoffer <cristhofferperezgaray@gmail.com>
2022-12-30 01:16:30 -0600

    Actualización de Requerimientos

--- config/settings.py
+++ config/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
